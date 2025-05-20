b15c51dad79c8d6279e60f203b46a79845763e2c
Kenchin-Pro <quentin.guyot@edu.esiee.fr>
2025-05-20 11:04:00 +0200

    ajout des classes Classes.cs et Eleve.cs

---
+++ Classe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Classe
+    {
+        public String nomClasse;
+        public List<Eleve> eleves = new List<Eleve>();
+        public List<String> matieres = new List<String>();
+
+        public Classe(String nc)
+        {
+            nomClasse = nc;
+        }
+
+        public void ajouterEleve(String prenom, String nom)
+        {
+            Eleve eleve = new Eleve(prenom, nom);
+            eleves.Add(eleve);
+        }
+        public void ajouterMatiere(String nom)
+        { 
+            matieres.Add(nom);
+        }
+        public float moyenneMatiere(int matiere)
+        {
+            float moyenne = 0f;
+            for (int i = 0; i < eleves.Count; i++)
+            {
+                moyenne += eleves[i].moyenneMatiere(matiere);
+            }
+            return (float)Math.Truncate((moyenne /eleves.Count)*100)/100;
+        }
+        public float moyenneGeneral()
+        {
+            float moyenne = 0f;
+            for (int i = 0;i < matieres.Count; i++)
+            {
+                moyenne += moyenneMatiere(i);
+            }
+            return (float)Math.Truncate((moyenne / matieres.Count) * 100) / 100;
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Eleve
+    {
+        public String prenom { get; private set; }
+        public String nom { get; private set; }
+        public List<Note> notes = new List<Note>();
+
+        public Eleve(String p, String n)
+        {
+            prenom = p;
+            nom = n;
+        }
+        public void ajouterNote(Note note)
+        {
+            notes.Add(note);
+        }
+        public float moyenneMatiere(int numMatiere)
+        {
+            float c = 0f;
+            float moyenne = 0f;
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (notes[i].matiere == numMatiere)
+                {
+                    moyenne += notes[i].note;
+                    c++;
+                }
+            }
+            moyenne = moyenne / c;
+            return (float)Math.Truncate(moyenne*100)/100;
+        }
+        public float moyenneGeneral()
+        {
+            float c = 0f;
+            float moyenne = 0f;
+            List<int> matiere = new List<int>();
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (!matiere.Contains(notes[i].matiere))
+                {
+                    moyenne += moyenneMatiere(notes[i].matiere);
+                    matiere.Add(notes[i].matiere);
+                    c++;
+                }
+            }
+            return (float)Math.Truncate((moyenne/c)*100)/100;
+        }
+    }
+}
